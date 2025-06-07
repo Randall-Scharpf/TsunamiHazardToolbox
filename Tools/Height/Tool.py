9aa34fff3a80fd56acb28a92059a02036da40ee1
Randall Scharpf <randall.scharpf@gmail.com>
2025-06-07 10:47:02 -0700

    finish simulation code v1

--- Tools/Height/Tool.py
+++ Tools/Height/Tool.py
@@ -25,8 +25,17 @@ dip_angle = arcpy.GetParameter(4)
 slip_angle = arcpy.GetParameter(5)
 
 sim = FluidSimulation.FluidSimulation(topography)
-sim.add_timestep(60)
+epicenter_point = GeospatialFlattener.parse_point(epicenter)
+sim.add_earthquake(
+    epicenter_point,
+    float(magnitude),
+    float(fault_heading),
+    float(dip_angle),
+    float(slip_angle)
+)
 
-point = GeospatialFlattener.parse_point(epicenter)
-
-raise ValueError((sim.get_elevation(point), sim.get_wave_elevation(point)))
+fig, ax = plt.subplots()
+ax.imshow(sim.base_elevation + sim.water_height)
+# yx = self._longlat_to_yx(arcpy.Point(longitude, latitude))
+# ax.scatter(yx[1], yx[0], color='red', marker='o', s=25)
+plt.show()
--- Tools/Simulator/FluidSimulation.py
+++ Tools/Simulator/FluidSimulation.py
@@ -1,11 +1,10 @@
 import arcpy
 import numpy as np
 import matplotlib.pyplot as plt
-from math import floor
+import math
 from statistics import median
 
 from Tools.Simulator.EarthquakeGenerator import Earthquake
-from Tools.Simulator.GeospatialFlattener import parse_point
 
 class FluidSimulation:
     EARTH_RADIUS = 6378.1370
@@ -26,11 +25,65 @@ class FluidSimulation:
         self.accel_y = np.zeros_like(self.base_elevation)
 
     def add_earthquake(self, epicenter, magnitude, fault_heading, fault_dip, fault_slip):
-        pass
+        earthquake = Earthquake(magnitude, fault_heading, fault_dip, fault_slip)
+        epc_yx = self._longlat_to_yx(epicenter)
+        for y in range(self.shape[0]):
+            for x in range(self.shape[1]):
+                coord = (y,x)
+                r, heading = self._longlat_distance_heading(epicenter, self._yx_to_longlat(coord))
+                theta = math.pi / 180 * (90 - heading)
+                self.water_height[coord] += earthquake.get_uplift(x - epc_yx[0], y - epc_yx[1])
+
+    def _longlat_distance_heading(self, longlat_a, longlat_b):
+        long1 *= longlat_a.X * math.pi / 180
+        lat1 *= longlat_a.Y * math.pi / 180
+        long2 *= longlat_b.X * math.pi / 180
+        lat2 *= longlat_b.Y * math.pi / 180
+        x1 = math.sin(long1) * math.cos(lat1)
+        y1 = math.cos(long1) * math.cos(lat1)
+        z1 = math.sin(lat1)
+        x2 = math.sin(long2) * math.cos(lat2)
+        y2 = math.cos(long2) * math.cos(lat2)
+        z2 = math.sin(lat2)
+        vecang = math.acos(x1*x2 + y1*y2 + z1*z2)
+        return (vecang * self.EARTH_RADIUS, 0)
 
     def add_timestep(self, delta):
-        # delta in seconds
-        pass
+        for y in range(1, self.shape[0] - 1):
+            for x in range(1, self.shape[1] - 1):
+                surface_gradient = (
+                    (self.base_elevation[y,x+1] + self.water_height[y,x+1]) - (self.base_elevation[y,x-1] + self.water_height[y,x-1]),
+                    (self.base_elevation[y+1,x] + self.water_height[y+1,x]) - (self.base_elevation[y-1,x] + self.water_height[y-1,x])
+                )
+                drag = (
+                    -0.01 * math.hypot(self.velocity_x[y,x], self.velocity_y[y,x]) / self.water_height[y,x] * self.velocity_x,
+                    -0.01 * math.hypot(self.velocity_x[y,x], self.velocity_y[y,x]) / self.water_height[y,x] * self.velocity_y,
+                )
+                self.accel_x[y,x] = 9.8 * surface_gradient[0] + drag[0]
+                self.accel_y[y,x] = 9.8 * surface_gradient[1] + drag[1]
+        change_in_height = np.zeros_like(self.water_height)
+        for y in range(self.shape[0]):
+            for x in range(self.shape[1]):
+                yy = y + 0.001 * (delta * self.velocity_y[y, x] + 0.5 * delta ** 2 * self.accel_y[y, x])
+                xx = x + 0.001 * (delta * self.velocity_x[y, x] + 0.5 * delta ** 2 * self.accel_x[y, x])
+                change_in_height[y, x] -= self.water_height[y, x]
+                y_1 = median([int(math.floor(yy)), 0, self.shape[0]-1])
+                x_1 = median([int(math.floor(xx)), 0, self.shape[1]-1])
+                y_2 = median([int(math.floor(yy)) + 1, 0, self.shape[0]-1])
+                x_2 = median([int(math.floor(xx)) + 1, 0, self.shape[1]-1])
+                mod_y = (yy - int(math.floor(yy)))
+                mod_x = (xx - int(math.floor(xx)))
+                NW = (y_1, x_1)
+                SW = (y_2, x_1)
+                NE = (y_1, x_2)
+                SE = (y_2, x_2)
+                change_in_height[NW] += (1 - mod_y) * (1 - mod_x) * self.water_height[y, x]
+                change_in_height[SW] += (mod_y) * (1 - mod_x) * self.water_height[y, x]
+                change_in_height[NE] += (1 - mod_y) * (mod_x) * self.water_height[y, x]
+                change_in_height[SE] += (mod_y) * (mod_x) * self.water_height[y, x]
+        self.water_height += change_in_height
+        self.velocity_x += self.accel_x * delta
+        self.velocity_y += self.accel_y * delta
 
     def get_elevation(self, latlong):
         return self._interpolate(
@@ -45,16 +98,16 @@ class FluidSimulation:
         )
 
     def _interpolate(self, arr, index):
-        y = median([int(floor(index[0])), 0, arr.shape[0]-1])
-        x = median([int(floor(index[1])), 0, arr.shape[1]-1])
-        y_2 = median([int(floor(index[0])) + 1, 0, arr.shape[0]-1])
-        x_2 = median([int(floor(index[1])) + 1, 0, arr.shape[1]-1])
+        y = median([int(math.floor(index[0])), 0, arr.shape[0]-1])
+        x = median([int(math.floor(index[1])), 0, arr.shape[1]-1])
+        y_2 = median([int(math.floor(index[0])) + 1, 0, arr.shape[0]-1])
+        x_2 = median([int(math.floor(index[1])) + 1, 0, arr.shape[1]-1])
         NW = (y, x)
         SW = (y_2, x)
         NE = (y, x_2)
         SE = (y_2, x_2)
-        mod_y = (index[0] - int(floor(index[0])))
-        mod_x = (index[1] - int(floor(index[1])))
+        mod_y = (index[0] - int(math.floor(index[0])))
+        mod_x = (index[1] - int(math.floor(index[1])))
         return (
             (1 - mod_y) * (1 - mod_x) * arr[NW] +
             mod_y * (1 - mod_x) * arr[SW] +
